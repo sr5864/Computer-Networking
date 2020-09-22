923f318a6942c28dd9db121ecf93206322f7dacc
sr5864 <71721037+sr5864@users.noreply.github.com>
2020-09-22 15:35:31 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,9 +12,9 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-        answer = hashlib.md5(b'NYU Computer Networking')
+        answer = hashlib.md5('NYU Computer Networking')
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
