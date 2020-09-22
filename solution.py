16e0863e8b490f1b2c1ecab57ddb85f4f148d649
sr5864 <71721037+sr5864@users.noreply.github.com>
2020-09-22 14:50:33 -0400

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,25 @@
+
+### welcome_assignment_answers
+### Input - All eight questions given in the assignment.
+### Output - The right answer for the specific question.
+
+def welcome_assignment_answers(question):
+    #The student doesn't have to follow the skeleton for this assignment.
+    #Another way to implement is using a "case" statements similar to C.
+    if question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    return(answer)
+# Complete all the questions.
+
+
+if __name__ == "__main__":
+    #use this space to debug and verify that the program works
+    debug_question = "Are encoding and encryption the same? - Yes/No"
+    print(debug_question)
+    print(welcome_assignment_answers(debug_question))
+    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
+    print(debug_question)
+    print(welcome_assignment_answers(debug_question))
+
