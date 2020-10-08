72b0266c66f1ebaa58b8c5bd1e0dd1ab42fa6a82
sr5864 <71721037+sr5864@users.noreply.github.com>
2020-10-07 20:00:52 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -18,6 +18,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     recv = clientSocket.recv(1024).decode()
     #print(recv)
     if recv[:3] != '220':
+        pass
         #print('220 reply not received from server.')
 
     # Send HELO command and print server response.
@@ -26,6 +27,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     recv1 = clientSocket.recv(1024).decode()
     #print(recv1)
     if recv1[:3] != '250':
+        pass
         #print('250 reply not received from server.')
 
     # Send MAIL FROM command and print server response.
