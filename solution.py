994b6f39eb032305d7d94bec0ab76b2b8a8e2471
sr5864 <71721037+sr5864@users.noreply.github.com>
2020-11-11 17:12:14 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -58,7 +58,6 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
             timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
             return timeReceived - timeSent
 
-        timeLeft = timeLeft - howLongInSelect
 
         # Fill in end
         timeLeft = timeLeft - howLongInSelect
