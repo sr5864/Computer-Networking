ff0ce16690f01ff74144e7bde6ad165d1d5900fa
sr5864 <71721037+sr5864@users.noreply.github.com>
2020-11-12 21:56:22 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,28 +5,22 @@ import struct
 import time
 import select
 import binascii
-# Should use stdev
-
 ICMP_ECHO_REQUEST = 8
-timeRTT = []
-packageSent = 0
-packageRev = 0
 
-
-def checksum(string):
+def checksum(str_):
+    # In this function we make the checksum of our packet 
+    str_ = bytearray(str_)
     csum = 0
-    countTo = (len(string) // 2) * 2
-    count = 0
+    countTo = (len(str_) // 2) * 2
 
-    while count < countTo:
-        thisVal = (string[count + 1]) * 256 + (string[count])
-        csum += thisVal
-        csum &= 0xffffffff
-        count += 2
+    for count in range(0, countTo, 2):
+        thisVal = str_[count+1] * 256 + str_[count]
+        csum = csum + thisVal
+        csum = csum & 0xffffffff
 
-    if countTo < len(string):
-        csum += (string[len(string) - 1])
-        csum &= 0xffffffff
+    if countTo < len(str_):
+        csum = csum + str_[-1]
+        csum = csum & 0xffffffff
 
     csum = (csum >> 16) + (csum & 0xffff)
     csum = csum + (csum >> 16)
@@ -35,50 +29,36 @@ def checksum(string):
     answer = answer >> 8 | (answer << 8 & 0xff00)
     return answer
 
-
-
 def receiveOnePing(mySocket, ID, timeout, destAddr):
-    global packageRev, timeRTT
     timeLeft = timeout
-
     while 1:
         startedSelect = time.time()
         whatReady = select.select([mySocket], [], [], timeLeft)
         howLongInSelect = (time.time() - startedSelect)
-        if whatReady[0] == []:  # Timeout
+        if whatReady[0] == []: # Timeout
             return "Request timed out."
 
         timeReceived = time.time()
         recPacket, addr = mySocket.recvfrom(1024)
 
-        # Fill in start
-
-        # Fetch the ICMP header from the IP packet
         icmpHeader = recPacket[20:28]
-        (requestType, code, revChecksum, revId, revSequence) = \
-            struct.unpack('bbHHh', icmpHeader)
-        if ID == revId:
-            bytesInDouble = struct.calcsize('d')
-            timeData = struct.unpack('d', recPacket[28:28
-                                                       + bytesInDouble])[0]
-            timeRTT.append(timeReceived - timeData)
-            packageRev += 1
-            return timeReceived - timeData
-        else:
-            return 'ID is not the same!'
+        icmpType, code, mychecksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
+    
+        if type != 8 and packetID == ID:
+            bytesInDouble = struct.calcsize("d")
+            timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
+            return timeReceived - timeSent
 
-        # Fill in end
         timeLeft = timeLeft - howLongInSelect
+        
         if timeLeft <= 0:
             return "Request timed out."
 
-
 def sendOnePing(mySocket, destAddr, ID):
-    global packageSent
     # Header is type (8), code (8), checksum (16), id (16), sequence (16)
 
     myChecksum = 0
-    # Make a dummy header with a 0 checksum
+    # Make a dummy header with a 0 checksum.
     # struct -- Interpret strings as packed binary data
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     data = struct.pack("d", time.time())
@@ -86,51 +66,39 @@ def sendOnePing(mySocket, destAddr, ID):
     myChecksum = checksum(header + data)
 
     # Get the right checksum, and put in the header
-
     if sys.platform == 'darwin':
-        # Convert 16-bit integers from host to network  byte order
         myChecksum = htons(myChecksum) & 0xffff
+    #Convert 16-bit integers from host to network byte order.
     else:
         myChecksum = htons(myChecksum)
 
-
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     packet = header + data
+    mySocket.sendto(packet, (destAddr, 1)) # AF_INET address must be tuple, not str
+    #Both LISTS and TUPLES consist of a number of objects
+    #which can be referenced by their position number within the object
 
-    mySocket.sendto(packet, (destAddr, 1))  # AF_INET address must be tuple, not str
-    packageSent += 1
+def doOnePing(destAddr, timeout):         
+    icmp = getprotobyname("icmp") 
+    #Create Socket here
+    mySocket = socket(AF_INET, SOCK_DGRAM, icmp) 
 
-    # Both LISTS and TUPLES consist of a number of objects
-    # which can be referenced by their position number within the object.
+    myID = os.getpid() & 0xFFFF  #Return the current process i     
+    sendOnePing(mySocket, destAddr, myID) 
+    delay = receiveOnePing(mySocket, myID, timeout, destAddr)          
 
-def doOnePing(destAddr, timeout):
-    icmp = getprotobyname("icmp")
-
-
-    # SOCK_RAW is a powerful socket type. For more details:   http://sockraw.org/papers/sock_raw
-    mySocket = socket(AF_INET, SOCK_RAW, icmp)
-
-    myID = os.getpid() & 0xFFFF  # Return the current process i
-    sendOnePing(mySocket, destAddr, myID)
-    delay = receiveOnePing(mySocket, myID, timeout, destAddr)
-    mySocket.close()
-    return delay
-
+    mySocket.close()         
+    return delay  
 
 def ping(host, timeout=1):
-    # timeout=1 means: If one second goes by without a reply from the server,  	# the client assumes that either the client's ping or the server's pong is lost
     dest = gethostbyname(host)
-    print("Pinging " + dest + " using Python:")
-    print("")
-    # Calculate vars values and return them
-    #  vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
-    # Send ping requests to a server separated by approximately one second
-    for i in range(0,4):
+    print ("Pinging " + dest + " using Python:")
+    print ("")
+    #Send ping requests to a server separated by approximately one second
+    while 1 :
         delay = doOnePing(dest, timeout)
-        print(delay)
-        time.sleep(1)  # one second
+        print (delay)
+        time.sleep(1)# one second
+    return delay
 
-    return vars
-
-if __name__ == '__main__':
-    ping("google.co.il")
+ping("google.co.il")
