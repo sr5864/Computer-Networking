e5a596295ec3da5d0a59179d800baa21999c641b
sr5864 <71721037+sr5864@users.noreply.github.com>
2020-11-12 22:07:58 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -54,7 +54,7 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         if packetID == ID:
             bytesInDouble = struct.calcsize("d")
             timeSent = struct.unpack("d", recPacket[28:28 + bytesInDouble])[0]
-            return timeReceived - timeSent
+            return (timeReceived * 1000) - (timeSent * 1000)
 
 
 
