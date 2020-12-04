6dca0378594903941d109cbe9ca517c339517c71
sr5864 <71721037+sr5864@users.noreply.github.com>
2020-12-04 15:43:27 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -142,7 +142,11 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
                     # You should add your responses to your lists here
-                    tracelist1.append(" %d %.0fms %s %s" % (ttl, (timeReceived - timeSent) * 1000, addr[0], dest_name))
+                    # tracelist1.append(" %d %.0fms %s %s" % (ttl, (timeReceived - timeSent) * 1000, addr[0], dest_name))
+                    tracelist1.append("%d" % ttl)
+                    tracelist1.append("%.0fms" % (timeReceived - timeSent) * 1000)
+                    tracelist1.append("%s" % addr[0])
+                    tracelist1.append("%s" % dest_name)
                     tracelist2.append(tracelist1)
                     # Fill in end
                 elif types == 3:
@@ -150,7 +154,11 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
                     # You should add your responses to your lists here
-                    tracelist1.append(" %d %.0fms %s %s" % (ttl, (timeReceived - timeSent) * 1000, addr[0], dest_name))
+                    # tracelist1.append(" %d %.0fms %s %s" % (ttl, (timeReceived - timeSent) * 1000, addr[0], dest_name))
+                    tracelist1.append("%d" % ttl)
+                    tracelist1.append("%.0fms" % (timeReceived - timeSent) * 1000)
+                    tracelist1.append("%s" % addr[0])
+                    tracelist1.append("%s" % dest_name)
                     tracelist2.append(tracelist1)
                     # Fill in end
                 elif types == 0:
@@ -158,7 +166,11 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
                     # You should add your responses to your lists here and return your list if your destination IP is met
-                    tracelist1.append(" %d %.0fms %s %s" % (ttl, (timeReceived - timeSent) * 1000, addr[0], dest_name))
+                    # tracelist1.append(" %d %.0fms %s %s" % (ttl, (timeReceived - timeSent) * 1000, addr[0], dest_name))
+                    tracelist1.append("%d" % ttl)
+                    tracelist1.append("%.0fms" % (timeReceived - timeSent) * 1000)
+                    tracelist1.append("%s" % addr[0])
+                    tracelist1.append("%s" % dest_name)
                     tracelist2.append(tracelist1)
                     print(tracelist2)
                     return tracelist2
