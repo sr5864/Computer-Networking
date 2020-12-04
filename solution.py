b21826312dc394d177f1f1b41141cacfdbec55e8
sr5864 <71721037+sr5864@users.noreply.github.com>
2020-12-03 20:04:16 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -151,7 +151,7 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
                     # You should add your responses to your lists here
-                    tracelist1.append((str(ttl), str(round((timeReceived - t) * 1000))+"ms", addr[0],ip_to_host(addr[0]), dest_name))
+                    tracelist1.append((str(ttl), str(round((timeReceived - t) * 1000))+"ms", addr[0], dest_name))
                     tracelist2.append(tracelist1)
                     # Fill in end
                 elif types == 3:
@@ -159,7 +159,7 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
                     # You should add your responses to your lists here
-                    tracelist1.append((str(ttl), str(round((timeReceived - t) * 1000))+"ms", addr[0],ip_to_host(addr[0]), dest_name))
+                    tracelist1.append((str(ttl), str(round((timeReceived - t) * 1000))+"ms", addr[0], dest_name))
                     tracelist2.append(tracelist1)
                     # Fill in end
                 elif types == 0:
@@ -167,7 +167,7 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
                     # You should add your responses to your lists here and return your list if your destination IP is met
-                    tracelist1.append((str(ttl), str(round((timeReceived - t) * 1000))+"ms", addr[0],ip_to_host(addr[0]), dest_name))
+                    tracelist1.append((str(ttl), str(round((timeReceived - t) * 1000))+"ms", addr[0], dest_name))
                     tracelist2.append(tracelist1)
                     # print(tracelist2)
                     return tracelist2
